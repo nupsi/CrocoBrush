ac64642606e4b6af90e5b568c068e3c526e8e840
Nupsi <nupsi4.thenoob@gmail.com>
2019-04-10 17:28:33 +0300

    Added click event script to easily add mouse click events to objects with box colliders.

---
+++ CrocoBrush/Assets/CrocoBrush/Scripts/Events/ClickEvent.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace CrocoBrush.Events
+{
+    [RequireComponent(typeof(BoxCollider))]
+    public class ClickEvent : MonoBehaviour
+    {
+        [SerializeField]
+        private CustomEvent m_event;
+
+        private void OnMouseDown()
+        {
+            m_event.Invoke();
+        }
+    }
+}
