b50e9d460339176178bd45b16d016b47124a740e
Nupsi <nupsi4.thenoob@gmail.com>
2019-04-11 02:33:01 +0300

    Started implementing canvas manager to change between menus.

---
+++ CrocoBrush/Assets/CrocoBrush/Scripts/UI/Managers/RegisteredCanvas.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace CrocoBrush.UI
+{
+    [RequireComponent(typeof(Canvas))]
+    public class RegisteredCanvas : MonoBehaviour
+    {
+        [SerializeField]
+        private string m_name = "None";
+
+        private Canvas m_canvas;
+
+        private void Awake()
+        {
+            m_canvas = GetComponent<Canvas>();
+        }
+
+        public void OnEnable()
+        {
+            CanvasManager.Instance.RegisterCanvas(this);
+        }
+
+        public void OnDisable()
+        {
+            CanvasManager.Instance.RemoveCanvas(this);
+        }
+
+        public void Show(bool show)
+        {
+            m_canvas.enabled = show;
+            ShowChild(show);
+        }
+
+        private void ShowChild(bool show)
+        {
+            foreach(Transform child in transform)
+            {
+                print(child);
+                child.gameObject.SetActive(show);
+            }
+        }
+
+        public string Name => m_name;
+    }
+}
