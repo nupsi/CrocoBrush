739cdfe34becbd842874c330c0bb72ff9e8bfba2
Nupsi <nupsi4.thenoob@gmail.com>
2019-05-02 17:01:06 +0300

    Updated model and added menu ambience

---
+++ CrocoBrush/Assets/CrocoBrush/Scripts/Audio/AmbiencePlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace CrocoBrush
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class AmbiencePlayer : RegisteredBehaviour
+    {
+        private AudioSource m_source;
+
+        private void Awake()
+        {
+            m_source = GetComponent<AudioSource>();
+        }
+
+        protected void LevelStart() => m_source.Pause();
+
+        protected void LevelEnd() => m_source.Play();
+
+        protected override Dictionary<string, Action> Actions =>
+            m_actions ??
+            (m_actions = new Dictionary<string, Action>
+            {
+                        { "LevelStart", LevelStart },
+                        { "LevelEnd", LevelEnd }
+            });
+    }
+}
