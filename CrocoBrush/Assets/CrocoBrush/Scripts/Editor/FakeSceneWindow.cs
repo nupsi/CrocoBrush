45d525e875f759b75e13cac5fff861217b2a9ec9
Nupsi <nupsi4.thenoob@gmail.com>
2019-08-10 04:46:28 +0300

    Updated fake scene window to display activators

--- CrocoBrush/Assets/CrocoBrush/Scripts/Editor/FakeSceneWindow.cs
+++ CrocoBrush/Assets/CrocoBrush/Scripts/Editor/FakeSceneWindow.cs
@@ -1,15 +1,21 @@
 ﻿using CrocoBrush.Managers;
+using System.Reflection;
 using UnityEditor;
 using UnityEngine;
-using System;
-using System.Reflection;
 
 namespace CrocoBrush.Editors
 {
     public class FakeSceneWindow : EditorWindow
     {
+        private readonly BindingFlags m_flags = BindingFlags.Instance
+            | BindingFlags.NonPublic
+            | BindingFlags.Static;
+
         private FakeScene[] m_scenes;
+        private Activator[] m_activators;
         private Vector2 m_scroll;
+        private bool m_showScenes;
+        private bool m_showActivators;
 
         [MenuItem("CrocoBrush/FakeSceneViewer")]
         public static void ShowWindow()
@@ -17,34 +23,61 @@ namespace CrocoBrush.Editors
             GetWindow<FakeSceneWindow>("Fake Scene Viewer");
         }
 
-        private void OnEnable()
+        protected void OnEnable()
         {
             m_scroll = new Vector2();
         }
 
-        private void OnGUI()
+        protected void OnGUI()
+        {
+            Check();
+            m_scroll = EditorGUILayout.BeginScrollView(m_scroll);
+            Button("Find Fake Scenes", UpdateScenes);
+            Button("Find Activaros", UpdateActivators);
+            DrawSceneInfo();
+            DrawActivatorInfo();
+            EditorGUILayout.EndScrollView();
+        }
+
+        private void Check()
         {
             if(m_scenes == null)
             {
                 UpdateScenes();
             }
-            if(GUILayout.Button("Find Fake Scenes"))
+
+            if(m_activators == null)
             {
-                UpdateScenes();
+                UpdateActivators();
             }
+        }
+
+        private void DrawSceneInfo()
+        {
             if(m_scenes.Length == 0)
             {
                 EditorGUILayout.LabelField("No Fake Scenes", EditorStyles.boldLabel);
+            }
+            else
+            {
+                DrawFakeSceneList();
+            }
+        }
+
+        private void DrawFakeSceneList()
+        {
+            m_showScenes = EditorGUILayout.Foldout(m_showScenes, "Scenes");
+            if(!m_showScenes)
+            {
                 return;
             }
-            m_scroll = EditorGUILayout.BeginScrollView(m_scroll);
+
             foreach(var scene in m_scenes)
             {
-                var flags = BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Static;
                 var type = scene.GetType();
-                var sceneName = type.GetField("m_name", flags).GetValue(scene);
-                var behavious = type.GetField("m_behaviour", flags).GetValue(scene) as Behaviour[];
-                var gameObjects = type.GetField("m_gameObjects", flags).GetValue(scene) as GameObject[];
+                var sceneName = type.GetField("m_name", m_flags).GetValue(scene);
+                var behavious = type.GetField("m_behaviour", m_flags).GetValue(scene) as Behaviour[];
+                var gameObjects = type.GetField("m_gameObjects", m_flags).GetValue(scene) as GameObject[];
                 EditorGUILayout.ObjectField(scene.gameObject, typeof(GameObject), true);
                 EditorGUILayout.LabelField($"Scene Name: {sceneName}");
                 EditorGUILayout.BeginHorizontal();
@@ -54,12 +87,57 @@ namespace CrocoBrush.Editors
                 EditorGUILayout.LabelField($"Child Count: {scene.gameObject.transform.childCount}");
                 EditorGUILayout.Space();
             }
-            EditorGUILayout.EndScrollView();
         }
 
-        private void UpdateScenes()
+        private void DrawActivatorInfo()
         {
-            m_scenes = Resources.FindObjectsOfTypeAll<FakeScene>();
+            if(m_activators.Length == 0)
+            {
+                EditorGUILayout.LabelField("No Activaros in Scene", EditorStyles.boldLabel);
+            }
+            else
+            {
+                DrawActivatorList();
+            }
+        }
+
+        private void DrawActivatorList()
+        {
+            m_showActivators = EditorGUILayout.Foldout(m_showActivators, "Activators");
+            if(!m_showActivators)
+            {
+                return;
+            }
+
+            foreach(var activator in m_activators)
+            {
+                var type = activator.GetType();
+                var separate = (bool)type.GetField("m_separate", m_flags).GetValue(activator);
+                var name = (string)type.GetField("m_name", m_flags).GetValue(activator);
+                var canvas = separate ? (string)type.GetField("m_canvas", m_flags).GetValue(activator) : name;
+                var position = separate ? (string)type.GetField("m_position", m_flags).GetValue(activator) : name;
+                var text = name.Length + canvas.Length + position.Length == 0
+                    ? "'Back'"
+                    : name == canvas && name == position
+                        ? $"{name}"
+                        : string.Format("{0} ({1}, {2})",
+                            name.Length == 0 ? "-" : name,
+                            canvas.Length == 0 ? "-" : canvas,
+                            position.Length == 0 ? "-" : position);
+                Button(text, () => Selection.activeGameObject = activator.gameObject);
+            }
+        }
+
+        private void UpdateScenes() => m_scenes = Resources.FindObjectsOfTypeAll<FakeScene>();
+
+        private void UpdateActivators() => m_activators = Resources.FindObjectsOfTypeAll<Activator>();
+
+        private void Button(string text, Action action)
+        {
+            if(GUILayout.Button(text))
+            {
+                action.Invoke();
+            }
         }
     }
 }
